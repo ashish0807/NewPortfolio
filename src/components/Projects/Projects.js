e6a95bb59f08a5f107f9622d73e08071eb02ca56
ashish0807 <ashishshrma.0807@gmail.com>
2024-07-24 13:05:18 +0530

    link added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Projects.css';
-import news from  "../../assets/news.jpg";
+import chat from "../../assets/letschat.jpg";
 import ecommerce from "../../assets/ecommerce.jpg";
-import weather from "../../assets/weather.jpg";
 import medico from "../../assets/Medico.png";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Projects = () => {
 
@@ -13,19 +14,38 @@ const Projects = () => {
     <h2 className='worksTitle'>My Projects</h2>
       <div className='worksImgs'>
       <figure>
-      <img src={news} alt="News" className='worksImg' />
-      <figcaption>News Website</figcaption>
+      <img src={chat} width="260px" alt="Letschat" className='worksImg'/>
+      <br/>
+      <a href="https://letschat-nfoy.onrender.com/login" target='_blank'  rel="noopener noreferrer">
+      <FaExternalLinkAlt size={20} color='#f3165c' cursor="pointer" />
+      </a>
+      {" "}
+      <a href="https://github.com/ashish0807/Letschat" target='_blank'  rel="noopener noreferrer">
+      <FaGithub size={20} color='#f3165c' cursor="pointer" />
+      </a>
+      <br/>
+      <figcaption>LetsChat-Realtime Chat-App</figcaption>
       </figure>
       <figure>
       <img src={ecommerce} alt="Ecommerce" className='worksImg' />
+      <br/>
+      <FaExternalLinkAlt size={20} color='#f3165c' cursor="pointer" />
+      <br/>
       <figcaption>Ecommerce Platform</figcaption>
       </figure>
-      <figure>
+      {/* <figure>
       <img src={weather} alt="Weather" className='worksImg' />
       <figcaption>Weather App</figcaption>
-      </figure>
+      </figure> */}
       <figure>
       <img src={medico} alt="MedicalHealth" className='worksImg' />
+      <br/>
+      <FaExternalLinkAlt size={20} color='#f3165c' cursor="pointer" />
+      {" "}
+      <a href="https://github.com/ashish0807/Medico_health_app" target='_blank'  rel="noopener noreferrer">
+      <FaGithub size={20} color='#f3165c' cursor="pointer" />
+      </a>
+      <br/>
       <figcaption>Medical Health Recommendation</figcaption>
       </figure>
       </div>
